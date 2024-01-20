e089d3dff987024b1a389e98833aa1d323691ccb
Guy Cherubin <ngchrbn@gmail.com>
2024-01-20 06:26:49 +0200

    0x02: Update task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     else:
         sum = 0
         for i in range(1, len(sys.argv)):
-                sum += int(sys.argv[i])
+            sum += int(sys.argv[i])
         print(sum)
