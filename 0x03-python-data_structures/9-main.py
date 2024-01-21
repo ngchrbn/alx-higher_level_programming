623874df2ff688fb6751d0119873451b7a624ea0
Guy Cherubin <ngchrbn@gmail.com>
2024-01-21 15:35:07 +0200

    0x03: Update task 9

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -2,5 +2,11 @@
 max_integer = __import__('9-max_integer').max_integer
 
 my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+my_list_1 = []
+my_list_2 = [1, 30]
 max_value = max_integer(my_list)
+max_value1 = max_integer(my_list_1)
+max_value2 = max_integer(my_list_2)
 print("Max: {}".format(max_value))
+print("Max: {}".format(max_value1))
+print("Max: {}".format(max_value2))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        return "None"
     max_value = 0
     for num in my_list:
         if num > max_value:
