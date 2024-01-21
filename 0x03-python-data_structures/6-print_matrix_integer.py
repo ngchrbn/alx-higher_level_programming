c0f17b38bddb2b116e241a694a07324556062814
Guy Cherubin <ngchrbn@gmail.com>
2024-01-21 13:34:28 +0200

    0x03: Update task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if j == len(matrix[i]) - 1:
-                print(matrix[i][j])
+                print("{:d}".format(matrix[i][j]))
             else:
-                print(matrix[i][j], end=' ')
+                print("{:d}".format(matrix[i][j]), end=' ')
