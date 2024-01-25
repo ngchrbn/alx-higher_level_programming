1a9f5f2d1077dd116c8686b6f2455d4231381ee6
Guy Cherubin <ngchrbn@gmail.com>
2024-01-26 01:34:32 +0200

    0x04: Task 12: Correct style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_to_int = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_to_int = {
+            "I": 1, "V": 5, "X": 10, "L": 50,
+            "C": 100, "D": 500, "M": 1000}
     total = 0
     prev_value = 0
 
