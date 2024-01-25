782080aad8e95138a21cf15c8b4cb0ba16aa81b0
Guy Cherubin <ngchrbn@gmail.com>
2024-01-25 22:39:57 +0200

    0x04: Add task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/vin/python3
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+
+    Parameters:
+    a_dictionary (dict): The dictionary to count the keys of.
+
+    Returns:
+    int: The number of keys in the dictionary.
+    """
+    return len(a_dictionary)
