d4cd1f057fb2c6f6aa0855ec2a0bd2c245cc347e
Guy Cherubin <ngchrbn@gmail.com>
2024-01-26 02:05:45 +0200

    0x04: Task 13: Correct style

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    """returns the weighted average of all integers tuple (<score>, <weight>)"""
+    """
+    Returns the weighted average of all integers tuple (<score>, <weight>)
+    """
     if not my_list:
         return 0
 
