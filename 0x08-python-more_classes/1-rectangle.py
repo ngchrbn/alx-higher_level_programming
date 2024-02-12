d7c1dc7813702bda4778a431c72b0ce4c5db56d5
Guy Cherubin <ngchrbn@gmail.com>
2024-02-12 16:35:38 +0200

    0x08: T1: Add docstring to __init__

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,6 +6,12 @@
 class Rectangle:
     """Defines a Rectangle."""
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.__height = height
         self.__width = width
 
