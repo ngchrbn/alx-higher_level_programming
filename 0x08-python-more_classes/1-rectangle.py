ed5a95cbad9d0fda5bc20630203dec47463a29bf
Guy Cherubin <ngchrbn@gmail.com>
2024-02-12 16:34:10 +0200

    0x08: T1: Add docstring to methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,15 +6,17 @@
 class Rectangle:
     """Defines a Rectangle."""
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
+        """Returns width."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set width."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Returns height."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set height."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
