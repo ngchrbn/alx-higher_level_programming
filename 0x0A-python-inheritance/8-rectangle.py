e73dc95cedba94fcec1f3027991a209ebf9e13bf
Guy Cherubin <ngchrbn@gmail.com>
2024-02-21 18:36:48 +0200

    0x0B: T8: Update code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -16,11 +16,7 @@ class Rectangle(BaseGeometry):
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
         """
-        try:
-            self.integer_validator("width", width)
-            self.integer_validator("height", height)
-        except (TypeError, ValueError) as e:
-            print(e)
-        else:
-            self.__width = width
-            self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
