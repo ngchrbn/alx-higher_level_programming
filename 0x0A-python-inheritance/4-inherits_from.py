cca4e141e52c64cfee00e840432d741422b0379c
Guy Cherubin <ngchrbn@gmail.com>
2024-02-21 15:03:11 +0200

    0x0B: Update task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,5 +5,15 @@ Contains the inherits_from function
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """Checks if an object is an instance of a class that inherited
+    from the specified class.
+
+    Args:
+        obj (object): The object to check.
+        a_class (class): The specified class.
+        
+    Returns:
+        bool: True if obj is an instance of a class that directly or
+        indirectly inherited from a_class, False otherwise.
+    """
+    return isinstance(obj, a_class) and obj.__class__ != a_class
